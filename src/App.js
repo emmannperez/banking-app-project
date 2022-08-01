208263b0d86cdcc7f25da7f39880c2d43926a7b2
Princess Tamayao <tammytamayao@gmail.com>
2022-08-01 21:13:09 +0800

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import LoginForm from './Components/LoginForm';
+import NewAcct from './Components/NewAcct';
+import DashBoard from './Components/DashBoard';
+import HomePage from './Components/HomePage';
+import ContactUs from './Components/ContactUs';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/ContactUs" element={<ContactUs/>}/>
+      <Route path="/DashBoard" element={<DashBoard/>}/>
+      <Route path="/LoginForm" element={<LoginForm/>}/>
+      <Route path="/NewAcct" element={<NewAcct/>}/>
+    </Routes>
     </div>
   );
 }
