ccdabc6a54937bec79c4e2088ef5edd0bb407f0c
Princess Tamayao <tammytamayao@gmail.com>
2022-08-01 21:10:14 +0800

    Updates

---
+++ precache-manifest.e8eac65e9a32653e9266afb511e18756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac5ccf8a6c353ec0edd2",
+    "url": "/banking-app-project/static/css/main.badcbcb7.chunk.css"
+  },
+  {
+    "revision": "ac5ccf8a6c353ec0edd2",
+    "url": "/banking-app-project/static/js/main.ac5ccf8a.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/banking-app-project/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "de6c555acef12b327ae0",
+    "url": "/banking-app-project/static/js/2.de6c555a.chunk.js"
+  },
+  {
+    "revision": "579f80595e0dae588b19",
+    "url": "/banking-app-project/static/js/runtime~main.579f8059.js"
+  },
+  {
+    "revision": "104e4c84a991a662b96c9aeda3fef875",
+    "url": "/banking-app-project/index.html"
+  }
+];
