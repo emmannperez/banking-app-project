a084733c33d35e86e108031b3aa2196437da6dd3
Princess Tamayao <tammytamayao@gmail.com>
2022-08-02 22:36:56 +0800

    NewAcct-Update

--- src/Components/NewAcct.js
+++ src/Components/NewAcct.js
@@ -1,20 +1,74 @@
-import React, { useState, useEffect } from 'react'
+import React, {useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import NavBar from "./NavBar";
 
-
-
 function NewAcct (){
+const navigate=useNavigate()
+const [userName,setUserName]=useState('');
+const [email,setEmail]=useState('');
+const [password,setPassword]=useState('');
+
+const handleInputChange=(event)=> {
+  const {id,value} = event.target;
+  if(id==="userName"){
+    setUserName(value);
+  }
+  if(id==='email'){
+    setEmail(value);
+  }
+  if(id==='password'){
+    setPassword(value);
+  }
+}
+
+const prevAccountList = JSON.parse(localStorage.getItem('accountList')) || [];
+const [accountList,setAccountList]=useState(prevAccountList);
+const [successNewAcct,setSuccessNewAcct]=useState(false);
+const handleSubmitEvent=()=> {
+const userlogin=accountList.find(id =>
+  id.email===email && id.password===password
+);
+  
+if(userlogin===undefined){
+  alert("Successfully Created an Account!");
+  return navigate("/LogInForm");
+}else {
+  alert('Account Already Exists');
+}
+
+const acctData = {
+  username: userName,
+  email: email,
+  password: password
+};
+setAccountList(accountList.push(acctData));
+localStorage.setItem('accountList',JSON.stringify(accountList));
+setAccountList(accountList);
+setSuccessNewAcct(true);
+}
+
+useEffect(()=>{
+  if(successNewAcct){
+    setUserName('');
+    setEmail('');
+    setPassword('');
+  }
+  setSuccessNewAcct(false);
+},[successNewAcct])
+
 const regForm = (
 <div>
-    <form>
-    <div><label>First Name: </label><input name='firstName' type='text' placeholder='Enter your first name' required/></div>
-    <div><label>Last Name: </label><input name='lastName' type='text' placeholder='Enter your last name' required /></div>
-    <div><label>E-mail: </label><input name='email' type='email' placeholder='Enter your email' required /></div>
-    <div><label>Password: </label><input name='password' type='password' placeholder='Enter your password' required /></div>
-    <div><label>Confirm Password: </label><input name='cpassword' type='password' placeholder='Enter your password' required /></div>
-    <div><input type="submit" value='Create New Account' /></div>
-    </form>
+    <div className='NewAcctForm-container'>
+    <div><label>Username: </label><input id='userName' value={userName} onChange={(event)=>handleInputChange(event)} type='text' placeholder='Enter your username' /></div>
+    <div><label>E-mail: </label><input id='email' value={email} onChange={(event)=>handleInputChange(event)} type='email' placeholder='Enter your email'/></div>
+    <div><label>Password: </label><input id='password' value={password} onChange={(event)=>handleInputChange(event)} type='password' placeholder='Enter your password'/></div>
+    <div>
+    <div>User Type:</div>
+      <label><input type="radio" id="employee" /*onClick={handleUserType}*/ value='employee' required/>I'm an Employee</label>
+      <label><input type="radio" id="client" /*onClick={handleUserType}*/ value='client' required/>I'm a Client</label>
+    </div>
+    <div><input disabled={!userName || !email || !password} type="submit" value='Create' onClick={()=>handleSubmitEvent()}/></div>
+</div>
 </div>
 )
 
