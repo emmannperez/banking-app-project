de13f6737823f2289a6e3b7e39d0a85c05545056
Princess Tamayao <tammytamayao@gmail.com>
2022-08-02 22:36:40 +0800

    LogInForm-Update

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,90 +1,66 @@
 import React, { useState } from "react";
 import {useNavigate} from "react-router-dom";
 import NavBar from "./NavBar";
-
+  
 function LoginForm() {
   
-// User Login info
-const database = [
-    {
-      email: "user1@gmail.com",
-      password: "pass1"
-    },
-    {
-        email: "user2@gmail.com",
-      password: "pass2"
+  const [email,setEmail]=useState('');
+  const [password,setPassword]=useState('');
+  const navigate=useNavigate();
+  
+  const handleInputChange=(event)=> {
+    const {id,value} = event.target;
+    if(id==='email'){
+      setEmail(value);
     }
-  ];
-
-const errors = {
-    email: "invalid email",
-    pass: "invalid password"
-  };
-
-// React States
-const [errorMessages, setErrorMessages] = useState({});
-const [isSubmitted, setIsSubmitted] = useState(false);
-const navigate = useNavigate();
-
-
-const handleSubmit = (submitevent) => {
-//Prevent page reload
-    submitevent.preventDefault();
-
-var { email, pass } = document.forms[0];
-
-// Find user login info
-    const userData = database.find((user) => user.email === email.value);
-
-// Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        setErrorMessages({ name: "pass", message: errors.pass }); // Invalid password
-      } else {
-        setIsSubmitted(true);
-        navigate("/DashBoard");
-      }
-    } else {// Username not found
-      setErrorMessages({ name: "email", message: errors.email });
+    if(id==='password'){
+      setPassword(value);
     }
-  };
+  }
+  
+  const handleSubmit=(event)=> {
+  const database=JSON.parse(localStorage.getItem('accountList'));
+  event.preventDefault();
+  
+  const userlogin=database.find(id =>
+    id.email===email && id.password===password
+  );
 
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-
-  // JSX code for login form
+  if(userlogin===undefined){
+    alert("Account Does Not Exist!");
+  }else {
+    alert('Successfully Logged-In');
+    return navigate("/DashBoardEmployee");
+  }
+  }
+  
   const renderForm = (
     <div>
-      <form onSubmit={handleSubmit}> 
-        <div>
-          <label>Email </label>
-          <input type="email" name="email" required />
-          {renderErrorMessage("email")}
-        </div>
-        <div>
-          <label>Password </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
-        </div>
-        <div>
-          <input type="submit" value='Log in to your Account' />
-        </div>
-      </form>
-    </div>
-  );
-
-  return (
-    <div className="LoginForm-container">
-        <NavBar />
-      <div className="LoginForm">
-        <div className="LoginForm-title">Sign In</div>
-        {isSubmitted ? <div></div> : renderForm}
+    <div></div>
+    <form> 
+      <div><label>Email </label><input type="email" id="email" value={email} onChange={(event)=>handleInputChange(event)} required /></div>
+      <div><label>Password </label><input type="password" id="password" value={password} onChange={(event)=>handleInputChange(event)} required /></div>
+      <div>
+        <div>User Type:</div>
+        <label><input type="radio" id="employee" /*onClick={handleUserType}*/ value='employee' required/>I'm an Employee</label>
+        <label><input type="radio" id="client" /*onClick={handleUserType}*/ value='client' required/>I'm a Client</label>
       </div>
+      <div><input type="submit" disabled={!email || !password} onClick={handleSubmit} value='Log In' /></div>
+    </form>
     </div>
   );
-}
+  
+  return (
+  <div className="LoginForm-container">
+    <NavBar />
+    <div className="LoginForm">
+        <div className="LoginForm-title">Sign In</div>
+        {renderForm}
+    </div>
+  </div>
+  );
+  
+  }
 
 export default LoginForm;
+  
