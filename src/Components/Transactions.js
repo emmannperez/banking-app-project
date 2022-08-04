f3d32f600ef3bae1e569a716fc7ca33a531d8f67
Princess Tamayao <tammytamayao@gmail.com>
2022-08-04 16:16:04 +0800

    updated-transaction

---
+++ src/Components/Transactions.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBarEmployee from "./NavBarEmployee";
+
+function Transactions (){
+    return (
+        <div>
+        <NavBarEmployee/>
+        <div>Welcome to my Transactions!</div>
+        </div>
+    );
+}
+
+export default Transactions;
