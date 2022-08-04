f526e59524c3f58d7342564967db9a09e846e533
Princess Tamayao <tammytamayao@gmail.com>
2022-08-04 16:14:55 +0800

    update-manageaact.js

---
+++ src/Components/ManageAcct.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBarEmployee from "./NavBarEmployee";
+
+function ManageAcct () {
+    return (
+        <div>
+        <NavBarEmployee/>
+        <div>Welcome to Manage Accounts!</div>
+        </div>
+    );
+}
+
+export default ManageAcct;
