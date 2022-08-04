852a557848f85111ad531f33e3a8d395271be462
Princess Tamayao <tammytamayao@gmail.com>
2022-08-04 16:18:13 +0800

    updated-dashboardclient

--- src/Components/DashBoardEmployee.js
+++ src/Components/DashBoardEmployee.js
@@ -1,11 +1,18 @@
 import React from "react";
 import NavBarEmployee from "./NavBarEmployee";
+import DateTime from "./DateTime";
 
 function DashBoardEmployee () {
+    const currentUser = JSON.parse(localStorage.getItem('logInUser'));
+    const firstName=currentUser[currentUser.length-1].firstname;
+    const lastName=currentUser[currentUser.length-1].lastname;
+    
     return (
     <div>
-        <NavBarEmployee />
-        <div>Welcome to Employee Dashboard!</div>
+        <div><NavBarEmployee/></div>
+        <div>Welcome, <br/> {firstName} {lastName}!</div>
+        <div><DateTime/></div>
+        <div></div>
     </div>
     );
 }
