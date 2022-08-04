ff364bda03197002d529a9516d21826797162e77
Princess Tamayao <tammytamayao@gmail.com>
2022-08-04 16:21:32 +0800

    DashboardClient

---
+++ src/Components/DashBoardClient.js
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBarEmployee from "./NavBarEmployee";
+
+function DashBoardClient () {
+    return (
+    <div>
+        <NavBarEmployee/>
+        <div>Welcome to my Profile (Client)!</div>
+    </div>
+    );
+}
+
+export default DashBoardClient;
