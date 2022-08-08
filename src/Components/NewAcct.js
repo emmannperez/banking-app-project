0bfb498be591006a281f546b8246ace5f882d2ca
Princess Tamayao <tammytamayao@gmail.com>
2022-08-08 19:28:26 +0800

    updated new-acct

--- src/Components/NewAcct.js
+++ src/Components/NewAcct.js
@@ -1,17 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import NavBar from "./NavBar";
+import signUpImg from "./signUpImg.jpg";
 
 function NewAcct (){
 const navigate=useNavigate()
-const [userName,setUserName]=useState('');
+const [firstName,setfirstName]=useState('');
+const [lastName,setlastName]=useState('');
 const [email,setEmail]=useState('');
 const [password,setPassword]=useState('');
+const [usertype,setUserType]=useState('');
 
 const handleInputChange=(event)=> {
-  const {id,value} = event.target;
-  if(id==="userName"){
-    setUserName(value);
+  const {id,name,value} = event.target;
+  if(id==="firstName"){
+    setfirstName(value);
+  }
+  if(id==="lastName"){
+    setlastName(value);
   }
   if(id==='email'){
     setEmail(value);
@@ -19,37 +25,57 @@ const handleInputChange=(event)=> {
   if(id==='password'){
     setPassword(value);
   }
+  if(name==='usertype'){
+    setUserType(value);
+  }
 }
 
 const prevAccountList = JSON.parse(localStorage.getItem('accountList')) || [];
-const [accountList,setAccountList]=useState(prevAccountList);
+const prevClientList = JSON.parse(localStorage.getItem('clientList')) || [];
 const [successNewAcct,setSuccessNewAcct]=useState(false);
+const [accountList,setAccountList]=useState(prevAccountList);
+const [clientList,setClientList]=useState(prevClientList);
+
 const handleSubmitEvent=()=> {
+
+  const acctData = {
+    firstname: firstName,
+    lastname: lastName,
+    email: email,
+    password: password,
+    usertype: usertype,
+    balance: 'N/A'
+  };
+
+  if(usertype==='client') {
+  acctData.balance=0;
+  clientList.push(acctData);
+  localStorage.setItem('clientList',JSON.stringify(clientList));
+  setClientList(clientList);
+  }
+    
 const userlogin=accountList.find(id =>
-  id.email===email && id.password===password
+  id.email===email || id.password===password
 );
-  
+
 if(userlogin===undefined){
   alert("Successfully Created an Account!");
+  setAccountList(accountList.push(acctData));
+  localStorage.setItem('accountList',JSON.stringify(accountList));
+  setAccountList(accountList);
+  setSuccessNewAcct(true);
+
   return navigate("/LogInForm");
 }else {
   alert('Account Already Exists');
 }
 
-const acctData = {
-  username: userName,
-  email: email,
-  password: password
-};
-setAccountList(accountList.push(acctData));
-localStorage.setItem('accountList',JSON.stringify(accountList));
-setAccountList(accountList);
-setSuccessNewAcct(true);
 }
 
 useEffect(()=>{
   if(successNewAcct){
-    setUserName('');
+    setfirstName('');
+    setlastName('');
     setEmail('');
     setPassword('');
   }
@@ -58,16 +84,28 @@ useEffect(()=>{
 
 const regForm = (
 <div>
-    <div className='NewAcctForm-container'>
-    <div><label>Username: </label><input id='userName' value={userName} onChange={(event)=>handleInputChange(event)} type='text' placeholder='Enter your username' /></div>
-    <div><label>E-mail: </label><input id='email' value={email} onChange={(event)=>handleInputChange(event)} type='email' placeholder='Enter your email'/></div>
-    <div><label>Password: </label><input id='password' value={password} onChange={(event)=>handleInputChange(event)} type='password' placeholder='Enter your password'/></div>
+<div className='regForm-container'>
+  <div className='NewAcctLeft'>
+  <div className='NewAcctImage'><img src={signUpImg} id='NAimg' alt='HPIcon'/></div>
+  </div>
+  <div className='NewAcctRight'>
+    <div className='NewAcctRight-container'>
+    <div><span className='logoBlackM'>SIGN </span><span className='logoGoldM'>UP</span></div>
+      <div><label className='NewAcctLabel'>FIRST NAME </label><br/><input id='firstName' value={firstName} onChange={(event)=>handleInputChange(event)} type='text' placeholder='Enter your first name' /></div>
+      <div><label className='NewAcctLabel'>LAST NAME </label><br/><input id='lastName' value={lastName} onChange={(event)=>handleInputChange(event)} type='text' placeholder='Enter your last name' /></div>
+      <div><label className='NewAcctLabel'>E-MAIL: </label><br/><input id='email' value={email} onChange={(event)=>handleInputChange(event)} type='email' placeholder='Enter your email'/></div>
+      <div><label className='NewAcctLabel'>PASSWORD: </label><br/><input id='password' value={password} onChange={(event)=>handleInputChange(event)} type='password' placeholder='Enter your password'/></div>
     <div>
-    <div>User Type:</div>
-      <label><input type="radio" id="employee" /*onClick={handleUserType}*/ value='employee' required/>I'm an Employee</label>
-      <label><input type="radio" id="client" /*onClick={handleUserType}*/ value='client' required/>I'm a Client</label>
+    <div className='NewAcctLabel'>User Type:</div>
+      <div className='radio-container'>
+      <label><input type="radio" id="employee" name='usertype'  value='employee' onChange={(event)=>handleInputChange(event)} required/>I'm an <span> Employee </span></label>
+      <label><input type="radio" id="client" name='usertype'  value='client' onChange={(event)=>handleInputChange(event)} required/>I'm a <span>Client</span></label>
+      </div>
     </div>
-    <div><input disabled={!userName || !email || !password} type="submit" value='Create' onClick={()=>handleSubmitEvent()}/></div>
+    
+    <div className='NewAcctBtn-container'><input id='NewAcctBtn' disabled={!firstName || !lastName || !email || !password} type="submit" value='Create an Account' onClick={()=>handleSubmitEvent()}/></div>
+  </div>
+  </div>
 </div>
 </div>
 )
@@ -76,7 +114,6 @@ return (
     <div className="NewAcctForm-container">
         <NavBar />
       <div className="NewAcctForm">
-        <div className="NewAcct-title">New Account Page</div>
         {regForm}
       </div>
     </div>
