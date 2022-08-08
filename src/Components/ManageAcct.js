1ad5f1a9b6313ca1c705e30ad96c833083d3180e
Princess Tamayao <tammytamayao@gmail.com>
2022-08-08 19:27:14 +0800

    updated-manage-account

--- src/Components/ManageAcct.js
+++ src/Components/ManageAcct.js
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBarEmployee from "./NavBarEmployee";
+import { useNavigate } from "react-router-dom";
+import './ManageAcct.css';
+import MyProfile from "./MyProfile";
 
-function ManageAcct () {
-    return (
-        <div>
-        <NavBarEmployee/>
-        <div>Welcome to Manage Accounts!</div>
-        </div>
-    );
+
+const ManageAcct = () =>{
+const clientList = JSON.parse(localStorage.getItem('clientList'));
+const navigate = useNavigate();
+
+const handleNewAcct = (event) => {
+    event.preventDefault();
+    return navigate ('/AddClient');
 }
+
+  return (
+    <div className="DBE-container">
+    <div className="leftDBE-container"><NavBarEmployee/></div>
+    <div className="midDBE-container">
+      <div className="MA-body-container">
+        <h3>Manage Accounts</h3>
+        <table className="MA-table-container">
+        <tr className="MA-table-headers">
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Balance</th>
+        </tr>
+        {clientList.map((client) => {
+          const { firstname, lastname, email, balance } = client;
+          return (
+            <tr className="MA-table-data">
+              <td>{firstname}</td>
+              <td>{lastname}</td>
+              <td>{email}</td>
+              <td>PHP {balance}</td>
+            </tr>
+          );
+        })}
+        </table>
+        <button className="AddClientBtn" onClick={handleNewAcct}>Add New Acct</button>
+      </div>
+    </div>
+    <div className="rightDBE-container"><div><MyProfile/></div></div>
+    </div>
+  );
+};
 
 export default ManageAcct;
