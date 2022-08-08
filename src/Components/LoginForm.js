14235b5a7cb1fbeb5f4728bb78d25f3ec4cb1f5c
Princess Tamayao <tammytamayao@gmail.com>
2022-08-08 19:24:40 +0800

    updated-loginform

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {useNavigate} from "react-router-dom";
 import NavBar from "./NavBar";
+import logInImg from "./logInImg.jpg";
   
 function LoginForm() {
   
@@ -44,26 +45,30 @@ const handleInputChange=(event)=> {
   }
   
   const renderForm = (
-    <div>
-    <div></div>
-    <form> 
-      <div><label>Email </label><input type="email" id="email" value={email} onChange={(event)=>handleInputChange(event)} required /></div>
-      <div><label>Password </label><input type="password" id="password" value={password} onChange={(event)=>handleInputChange(event)} required /></div>
-      <div><input type="submit" disabled={!email || !password} onClick={handleSubmit} value='Log In' /></div>
-    </form>
+  <div className="renderForm-container">
+    <div className='LogInLeft'>
+    <div className='LogInLeft-container'> 
+      <div><span className='logoBlackM'>SIGN </span><span className='logoGoldM'>IN</span></div>
+      <div><label className='LogInLabel'>Email </label><input type="email" id="email" value={email} onChange={(event)=>handleInputChange(event)} required /></div>
+      <div><label className='LogInLabel'>Password </label><input type="password" id="password" value={password} onChange={(event)=>handleInputChange(event)} required /></div>
+      <div className='LogInBtn-container'><input id='LogInBtn' type="submit" disabled={!email || !password} onClick={handleSubmit} value='Log In' /></div>
     </div>
+    </div>
+    <div className='LogInRight'>
+    
+    <div className='LogInImage'><img src={logInImg} id='LIimg'/></div>
+    </div>
+  </div>
   );
   
   return (
   <div className="LoginForm-container">
     <NavBar />
     <div className="LoginForm">
-        <div className="LoginForm-title">Sign In</div>
         {renderForm}
     </div>
   </div>
   );
-  
   }
 
 export default LoginForm;
