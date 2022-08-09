ef4ba95ab8f21d4c859ab6f07bcc175becef9308
Princess Tamayao <tammytamayao@gmail.com>
2022-08-09 11:51:49 +0800

    updated-contactUs

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import NavBar from "./NavBar";
+import contactUsImg from "../contactUsVector.jpg";
 
 function ContactUs (){
     return (
-        <div>
-        <NavBar/>
-        <div>Welcome to Contact Us Page</div>
+        <div className='contactUsContainer'>
+            <NavBar />
+            <div className='contactUs-container'>
+                <div className='contactUsLeft'>
+                    <div className='contactUsImage'><img src={contactUsImg} id='CUimg' alt='HPIcon'></img></div>
+                </div>
+                <div className='contactUsRight'>
+                    <span className='logoBlackM'>CONTACT<span className='logoGoldM'> US</span></span>
+                    <div><label className="contactUslabel">NAME </label><br/><input id='contactUsName' type='text' placeholder='Enter your name' required /></div>
+                    <div><label className="contactUslabel">E-MAIL </label><br/><input id='contactUsEmail' type='email' placeholder='Enter your email' required /></div>
+                    <div><label className="contactUslabel">MESSAGE </label><br/><textarea id="contactUsMsg" rows='12'></textarea></div>
+                    <div className='contactUssubmit-container'><button className='contactUssubmit'>SEND US A MESSAGE</button></div>
+                </div>
+            </div>
         </div>
     );
 }
