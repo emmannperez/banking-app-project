5c0028a026ac8c705f1d395ab6f2b5dbc104814e
Princess Tamayao <tammytamayao@gmail.com>
2022-08-10 18:57:23 +0800

    updated-transaction-history

---
+++ src/Components/TransactHistory.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import NavBarClient from "./NavBarClient";
+
+function TransactHistory () {
+
+    return (
+        <div>
+        <div><NavBarClient/></div>
+        <div>Welcome to Transaction History!</div>
+        </div>
+    );
+
+}
+
+export default TransactHistory;
