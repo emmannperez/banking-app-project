cfebcef80e689583c64d3d3767ef8f04b01b77cb
emmannperez <ejn.perez@gmail.com>
2022-08-11 19:53:18 +0800

    Update NavBar.js

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () =>{
   <div className='navBar'>
       <li className='navComp'><Link to="/" className= 'navComp'><span className='logoBlackM'>BANK<span className='logoGoldM'>OK</span></span></Link></li>
       <li className='navComp'><Link to="/NewAcct" className='navCompLink'>SIGN UP</Link></li>
-      <li className='navComp'><Link to="/LoginForm" className='navCompLink'>SIGN IN</Link></li>
+      <li className='navComp'><Link to="/LoginForm" className='navCompLink'>LOG IN</Link></li>
       <li className='navComp'><Link to="/ContactUs" className='navCompLink'>CONTACT US</Link></li>
   </div>
   </div>
